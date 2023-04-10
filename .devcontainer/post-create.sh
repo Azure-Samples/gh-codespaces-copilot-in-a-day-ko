2cc3e5edf8e1667a7626b2fe33918a89ed2d01b8
Justin Yoo <justin.yoojh@gmail.com>
2023-04-10 02:48:34 +0000

    Add devcontainer settings

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,89 @@
+## Install additional apt packages
+sudo apt-get update \
+    && sudo apt upgrade -y \
+    && sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils \
+    && sudo apt clean -y \
+    && sudo rm -rf /var/lib/apt/lists/*
+
+## Configure git
+git config --global pull.rebase false
+git config --global core.autocrlf input
+
+## CaskaydiaCove Nerd Font
+# Uncomment the below to install the CaskaydiaCove Nerd Font
+mkdir $HOME/.local
+mkdir $HOME/.local/share
+mkdir $HOME/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip
+unzip CascadiaCode.zip -d $HOME/.local/share/fonts
+rm CascadiaCode.zip
+
+## GitHub Copilot CLI ##
+npm install -g @githubnext/github-copilot-cli
+eval "$(github-copilot-cli alias -- "$0")"
+
+## AZURE BICEP CLI ##
+az bicep install
+
+## AZURE FUNCTIONS CORE TOOLS ##
+npm i -g azure-functions-core-tools@4 --unsafe-perm true
+
+## Azurite ##
+npm install -g azurite
+
+## AZURE STATIC WEB APPS CLI ##
+npm install -g @azure/static-web-apps-cli
+
+## AZURE DEV CLI ##
+curl -fsSL https://aka.ms/install-azd.sh | bash
+
+## OH-MY-ZSH PLUGINS & THEMES (POWERLEVEL10K) ##
+git clone https://github.com/zsh-users/zsh-completions.git $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k --depth=1
+ln -s $HOME/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme $HOME/.oh-my-zsh/custom/themes/powerlevel10k.zsh-theme
+
+## OH-MY-ZSH - POWERLEVEL10K SETTINGS ##
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-zsh/.p10k-with-clock.zsh > $HOME/.p10k-with-clock.zsh
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-zsh/.p10k-without-clock.zsh > $HOME/.p10k-without-clock.zsh
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-zsh/switch-p10k-clock.sh > $HOME/switch-p10k-clock.sh
+chmod +x ~/switch-p10k-clock.sh
+
+cp $HOME/.p10k-with-clock.zsh $HOME/.p10k.zsh
+cp $HOME/.zshrc $HOME/.zshrc.bak
+
+echo "$(cat $HOME/.zshrc)" | awk '{gsub(/ZSH_THEME=\"devcontainers\"/, "ZSH_THEME=\"powerlevel10k\"")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
+echo "$(cat $HOME/.zshrc)" | awk '{gsub(/plugins=\(git\)/, "plugins=(git zsh-completions zsh-syntax-highlighting zsh-autosuggestions)")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
+echo "
+# To customize prompt, run 'p10k configure' or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+" >> $HOME/.zshrc
+
+## GitHub Copilot CLI ##
+echo '
+# Add GitHub Copilot CLI alias
+alias ghcp='github-copilot-cli'
+eval "$(github-copilot-cli alias -- "$0")"
+' >> $HOME/.zshrc
+echo '
+# Add GitHub Copilot CLI alias
+alias ghcp='github-copilot-cli'
+eval "$(github-copilot-cli alias -- "$0")"
+' >> $HOME/.bashrc
+
+## OH-MY-POSH ##
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+## OH-MY-POSH - POWERLEVEL10K SETTINGS ##
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/p10k-with-clock.omp.json > $HOME/p10k-with-clock.omp.json
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/p10k-without-clock.omp.json > $HOME/p10k-without-clock.omp.json
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/switch-p10k-clock.ps1 > $HOME/switch-p10k-clock.ps1
+
+mkdir $HOME/.config/powershell
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/Microsoft.PowerShell_profile.ps1 > $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
+curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/Microsoft.PowerShell_profile.ps1 > $HOME/.config/powershell/Microsoft.VSCode_profile.ps1
+
+cp $HOME/p10k-with-clock.omp.json $HOME/p10k.omp.json
