1bd2f847082430e06a3491c6533c560d35d65d23
Nayeon Kim <76769919+n-y-kim@users.noreply.github.com>
2023-04-18 14:58:51 +0000

    Added Spring boot & React

---
+++ spring-openai/src/main/java/com/example/springopenai/SpringOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springopenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenaiApplication.class, args);
+	}
+
+}
---
+++ spring-openai/src/test/java/com/example/springopenai/SpringOpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springopenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
