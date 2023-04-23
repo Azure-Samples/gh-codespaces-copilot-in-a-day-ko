33a42b3c1ac007ee6fbf58df249f65fd411b542a
Nayeon Kim <76769919+n-y-kim@users.noreply.github.com>
2023-04-23 07:43:13 +0000

    Change folder structure & ignore properties

---
+++ api/src/main/java/roadshow/demo/api/controller/HelloController.java
@@ -0,0 +1,28 @@
+package roadshow.demo.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+    @GetMapping("/hello")
+	public List<String> Hello(){
+        return Arrays.asList("Github Codespace 테스트", "Github Copilot 테스트");
+    }
+
+    // @GetMapping("/chat")
+    // public String chat(){
+    //     //return String from ApiApplication makeApiCall function
+        
+    // }
+
+}
