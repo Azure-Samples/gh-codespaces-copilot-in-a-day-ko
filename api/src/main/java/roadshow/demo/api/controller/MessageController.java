1ff6e7de83f47c5f39a4839c2d49ba5d26766250
Nayeon Kim <76769919+n-y-kim@users.noreply.github.com>
2023-05-09 05:02:27 +0000

    Update CORS

--- api/src/main/java/roadshow/demo/api/controller/MessageController.java
+++ api/src/main/java/roadshow/demo/api/controller/MessageController.java
@@ -33,6 +33,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @RequestMapping("/api/messages")
 public class MessageController {
 
+    //⬇️ copilot demo ⬇️
     @Value("${AOAI_API_ENDPOINT}")
     private String aoaiEndpoint;
 
@@ -44,6 +45,7 @@ public class MessageController {
 
     @Value("${AOAI_API_VERSION}")
     private String aoaiApiVersion;
+    //⬆️ copilot demo ⬆️
 
     private static final String ALLOWED_ORIGINS = "${CORS_ORIGIN}";
 
@@ -54,6 +56,7 @@ public class MessageController {
         summary = "Azure OpenAI API 질문 제출 및 답변 호출",
         description = "AOAI API(GPT 3.5)를 호출하여 질문을 text로 전달하고 content 답변을 받아 리턴합니다.",
 
+        //⬇️ copilot demo ⬇️
         requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
             description = "text 키 값으로 질문을 전달합니다.",
             required = true,
@@ -62,6 +65,7 @@ public class MessageController {
                 schema = @Schema(implementation = MessageRequest.class)
             )
         ),
+        //⬆️ copilot demo ⬆️
 
         responses = {
             @ApiResponse(responseCode = "200", description = "성공", content = {
