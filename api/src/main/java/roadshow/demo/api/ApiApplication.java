3fb570bc8508100e221ba2f8573a02a07ffd5f0b
Nayeon Kim <76769919+n-y-kim@users.noreply.github.com>
2023-05-09 05:34:07 +0000

    Revert to default

--- api/src/main/java/roadshow/demo/api/ApiApplication.java
+++ api/src/main/java/roadshow/demo/api/ApiApplication.java
@@ -2,10 +2,10 @@ package roadshow.demo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = ApiApplication.class)
+//@ComponentScan(basePackageClasses = ApiApplication.class)
 public class ApiApplication {
 
 	public static void main(String[] args) {
